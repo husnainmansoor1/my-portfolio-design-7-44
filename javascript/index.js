9a66ad207c53cadf1dc526811c47bd9f63748432
husnainmansoor1 <husnainmansoor948@gmail.com>
2024-11-14 19:45:11 +0500

    portfolio

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+
+//  sidebar
+
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener('click',()=>{
+  document.querySelector('.sidebar').classList.toggle('SidebarGo');
+  if (  document.querySelector('.sidebar').classList.contains('SidebarGo')){
+    document.querySelector('.ham').style.display='inline';
+    document.querySelector('.cross').style.display='none';
+  }
+  else{
+    document.querySelector('.ham').style.display='none';
+    setTimeout(()=>{
+      document.querySelector('.cross').style.display='inline';
+    },350);
+  }
+})
---
+++ javascript/type.js
@@ -0,0 +1,11 @@
+// type-script
+var typed = new Typed('#element1', {
+    strings: [' <i>Husnain Mansoor<i>.'],
+    typeSpeed: 50,
+    loop: true,
+  });
+var typed = new Typed('#element2', {
+    strings: ['<i> My Passion is Web Developer And Graphic Designer.<i>'],
+    typeSpeed: 50,
+    loop: true,
+  });
